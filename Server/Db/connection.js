0d6ce07a77bb539e84ecc154d4604356762b25b5
Priyanshu-Solanki <priyanshsolanki2015@gmail.com>
2024-09-23 21:51:07 +0530

    check

--- Server/Db/connection.js
+++ Server/Db/connection.js
@@ -1,17 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+const DB = process.env.DATABASE
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); 
-  }
-};
-
-module.exports = connectDB;
+mongoose.connect(DB).then(()=> console.log("database connected")).catch((error)=> console.log("error: " + error.message));
