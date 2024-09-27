75b4c23d9e97f63be9e5bebe444e5524b54f67de
Priyanshu-Solanki <priyanshsolanki2015@gmail.com>
2024-09-27 08:35:20 +0530

    deployed

--- Server/defaultData.js
+++ Server/defaultData.js
@@ -3,7 +3,7 @@ const productsdata = require("./Constant/productdata")
 
 const DefaultData = async () => {
     try {
-        // await Products.deleteMany({})
+        await Products.deleteMany({})
         const storeData = await Products.insertMany(productsdata)
         //console.log(storeData)
     } catch (error) {
